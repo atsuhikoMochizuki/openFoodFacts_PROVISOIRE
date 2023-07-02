b5644838d6509a529ba836bad27828d3afdc6947
arny-wilk <arnywilk@gmail.com>
2023-07-03 00:02:26 +0200

    Implement testing for DAO + services

---
+++ src/test/java/fr/diginamic/dal/DaoCategorieTest.java
@@ -0,0 +1,20 @@
+package fr.diginamic.dal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DaoCategorieTest {
+
+    @Test
+    public void ajouter() {
+    }
+
+    @Test
+    public void modifier() {
+    }
+
+    @Test
+    public void supprimer() {
+    }
+}
---
+++ src/test/java/fr/diginamic/service/ServicesTest.java
@@ -0,0 +1,32 @@
+package fr.diginamic.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServicesTest {
+
+    @Test
+    public void service1() {
+    }
+
+    @Test
+    public void service2() {
+    }
+
+    @Test
+    public void service3() {
+    }
+
+    @Test
+    public void service4() {
+    }
+
+    @Test
+    public void service5() {
+    }
+
+    @Test
+    public void service6() {
+    }
+}
